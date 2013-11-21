64f7bbe41947763a81253c77f55ce3e893594503
phanhung <phanhung0906@gmail.com>
2013-11-21 09:05:51 +0700

    #update session

--- src/index.php
+++ src/index.php
@@ -2,11 +2,6 @@
     define('ROOT_DIR', __DIR__);
     define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'test'));
     include_once ROOT_DIR . '/config/config.'.APPLICATION_ENV.'.php';
-    if(Sesion::has('user')){
-        if($_SERVER["REQUEST_URI"]=='/'){
-            header('location:http://'.ROOT_URL.'/'.Session::get('user'));
-        }
-    }
 
     require __DIR__.'/bootstrap/autoload.php';
 
