f4448cbc00b9c8559505dbf75e5226ad41666c4c
phanhung <phanhung0906@gmail.com>
2013-11-25 10:50:35 +0700

    # secure password

--- src/app/models/UserModel.php
+++ src/app/models/UserModel.php
@@ -1,16 +1,15 @@
 <?php
 Class UserModel
 {
-
+    const cryptString = '$2a$07$usesomesillystringforsalt$';
     public function register($userName, $password)
     {
         if ($userName == null || $userName == 'private' || $userName == 'public' || $userName == ' ')
             return 'error1';
         $results = DB::select('SELECT * FROM user WHERE user = ?', array($userName));
         if ($results == null) {
-            $temp = substr($password, strlen($password)-2, strlen($password));
-            $newPassword = $password.$temp;
-            DB::insert('INSERT INTO user (user, passwd) values (?, ?)', array($userName, md5($newPassword)));
+            $newPassword = crypt($password, UserModel::cryptString);
+            DB::insert('INSERT INTO user (user, passwd) values (?, ?)', array($userName, $newPassword));
             return 'login';
         }else
             return 'error2';
@@ -18,25 +17,23 @@ Class UserModel
 
     public function login($userName, $password)
     {
-        $temp = substr($password, strlen($password)-2, strlen($password));
-        $newPassword = $password.$temp;
-        $result = DB::select('SELECT * FROM user WHERE user = ? AND passwd = ?', array($userName, md5($newPassword)));
+        $cryptPassword = crypt($password, UserModel::cryptString);
+        $result = DB::select('SELECT * FROM user WHERE user = ?', array($userName));
         if (count($result) != 0) {
-            return true;
+            if ($cryptPassword == $result[0]->passwd)
+              return true;
         } else
             return false;
     }
 
     public function change($oldpass, $newpass, $confirm, $userName)
     {
-        $temp = substr($oldpass, strlen($oldpass)-2, strlen($oldpass));
-        $newPassword = $oldpass.$temp;
+        $newPassword = crypt($oldpass, UserModel::cryptString);
         $result = DB::select('SELECT * FROM user WHERE user = ?', array($userName));
-        if (md5($newPassword) == $result[0]->passwd) {
+        if ($newPassword == $result[0]->passwd) {
             if ($newpass == $confirm) {
-                $temp = substr($newpass, strlen($newpass)-2, strlen($newpass));
-                $newPasswordInsert = $newpass.$temp;
-                DB::update('UPDATE user SET passwd = ? WHERE user = ?', array(md5($newPasswordInsert), $userName));
+                $newPasswordInsert = crypt($newpass, UserModel::cryptString);
+                DB::update('UPDATE user SET passwd = ? WHERE user = ?', array($newPasswordInsert, $userName));
                 return true;
             }
         } else
