5318359a7d6bfefaa64116e4642597484403344a
phanhung <phanhung0906@gmail.com>
2013-11-25 15:55:24 +0700

    # coding

--- src/app/models/UserModel.php
+++ src/app/models/UserModel.php
@@ -1,26 +1,28 @@
 <?php
-Class UserModel
+
+class UserModel
 {
-    const EROR_EXIST_USER = -1;
-    const EROR_NAME_USER = -2;
+    const ERROR_EXIST_USER = -1;
+    const ERROR_NAME_USER = -2;
     const SUCCESS = -3;
     protected $passwordHash;
 
     public function __construct(){
-        $this->passwordHash = new PasswordHash(8, FALSE);
+        $this->passwordHash = new PasswordHash(8, false);
     }
 
     public function register($userName, $password)
     {
-        if ($userName == null || $userName == 'private' || $userName == 'public' || $userName == ' ')
+        if ($userName == null || $userName == 'private' || $userName == 'public' || $userName == ' ') {
             return UserModel::EROR_EXIST_USER;
+        }
         $results = DB::select('SELECT * FROM user WHERE user = ?', array($userName));
         if ($results == null) {
             $hashPassword =  $this->passwordHash->HashPassword($password);
             DB::insert('INSERT INTO user (user, passwd) values (?, ?)', array($userName, $hashPassword));
             return UserModel::SUCCESS;
-        }else
-            return UserModel::EROR_NAME_USER;
+        }
+        return UserModel::EROR_NAME_USER;
     }
 
     public function login($userName, $password)
@@ -33,9 +35,9 @@ Class UserModel
     {
         $result = DB::select('SELECT * FROM user WHERE user = ?', array($userName));
         if ($this->passwordHash->CheckPassword($oldpass, $result[0]->passwd)) {
-                $hashPassword =  $this->passwordHash->HashPassword($newpass);
-                DB::update('UPDATE user SET passwd = ? WHERE user = ?', array($hashPassword, $userName));
-                return true;
+            $hashPassword =  $this->passwordHash->HashPassword($newpass);
+            DB::update('UPDATE user SET passwd = ? WHERE user = ?', array($hashPassword, $userName));
+            return true;
         } else
             return false;
     }
