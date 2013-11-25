e60c4b65f31bc46542718fba92da4061e1912e52
phanhung <phanhung0906@gmail.com>
2013-11-25 15:38:15 +0700

    # password security

--- src/app/models/UserModel.php
+++ src/app/models/UserModel.php
@@ -1,6 +1,9 @@
 <?php
 Class UserModel
 {
+    const EROR_EXIST_USER = -1;
+    const EROR_NAME_USER = -2;
+    const SUCCESS = -3;
     protected $passwordHash;
 
     public function __construct(){
@@ -10,37 +13,29 @@ Class UserModel
     public function register($userName, $password)
     {
         if ($userName == null || $userName == 'private' || $userName == 'public' || $userName == ' ')
-            return 'error1';
+            return UserModel::EROR_EXIST_USER;
         $results = DB::select('SELECT * FROM user WHERE user = ?', array($userName));
         if ($results == null) {
             $hashPassword =  $this->passwordHash->HashPassword($password);
             DB::insert('INSERT INTO user (user, passwd) values (?, ?)', array($userName, $hashPassword));
-            return 'login';
+            return UserModel::SUCCESS;
         }else
-            return 'error2';
+            return UserModel::EROR_NAME_USER;
     }
 
     public function login($userName, $password)
     {
         $result = DB::select('SELECT * FROM user WHERE user = ?', array($userName));
-        if (count($result) != 0) {
-            $check =  $this->passwordHash->CheckPassword($password, $result[0]->passwd);
-            if ($check)
-                return true;
-        } else
-            return false;
+        return (count($result) != 0 && $this->passwordHash->CheckPassword($password, $result[0]->passwd));
     }
 
     public function change($oldpass, $newpass, $confirm, $userName)
     {
         $result = DB::select('SELECT * FROM user WHERE user = ?', array($userName));
-        $check =  $this->passwordHash->CheckPassword($oldpass, $result[0]->passwd);
-        if ($check){
-            if ($newpass == $confirm) {
+        if ($this->passwordHash->CheckPassword($oldpass, $result[0]->passwd)) {
                 $hashPassword =  $this->passwordHash->HashPassword($newpass);
                 DB::update('UPDATE user SET passwd = ? WHERE user = ?', array($hashPassword, $userName));
                 return true;
-            }
         } else
             return false;
     }
